36b203d1e527f5813d64370061188245aad09fa1
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-12-24 23:04:34 +0400

    run text rendering and calculations on different threads

--- visualizationwindow.h
+++ visualizationwindow.h
@@ -2,37 +2,33 @@
 #define VISUALIZATIONWINDOW_H
 
 #include <QMainWindow>
+#include <QThread>
+#include "worker.h"
 
-namespace Ui
-{
+namespace Ui {
 class VisualizationWindow;
 }
 
 class VisualizationWindow : public QMainWindow
 {
-  Q_OBJECT
+    Q_OBJECT
 
 public:
-  explicit VisualizationWindow(QWidget *parent = nullptr, QString firstNum = "",
-                               QString secondNum = "");
-  ~VisualizationWindow();
+    explicit VisualizationWindow(QWidget *parent = nullptr, QString firstNum = "", QString secondNum = "");
+    ~VisualizationWindow();
+
+signals:
+    void nextButtonClicked();
 
 private slots:
-  void on_nextButton_clicked();
+    void set_text(const QString &carryText, const QString &sumText);
+    void on_nextButton_clicked();
 
 private:
-  QString firstNumString;
-  QString secondNumString;
-  QString carryText = QString("%1").arg("", 7, ' ');
-  QString sumText = QString("%1").arg("", 7, ' ');
-  int i;
-  int sum = 0, carry = 0, prev_carry = 0;
-
-  void set_text();
-  void set_sum();
-  void set_carry();
-  void check_for_prev_carry();
-  Ui::VisualizationWindow *ui;
+    Ui::VisualizationWindow *ui;
+    Worker *worker;
+    QThread *workerThread;
 };
 
-#endif // VISUALIZATIONWINDOW_H
+#endif
+
